3d8d5525f93fc00d1c2142c9682db92449ac69ec
Craig Sykes <craig.sykes@musgrave.ie>
2024-10-16 21:01:08 +0100

    Ex1

--- Week_4_Java/Unit_4/EX_1/JS/main.js
+++ Week_4_Java/Unit_4/EX_1/JS/main.js
@@ -1,17 +1,17 @@
-// Declare variables
-let numberOfStars = 0;
-let outputStars = '';
+  // Declare variables
+  let numberOfStars = 0;
+  let outputStars = '';
 
-// inputs
-numberOfStars = prompt("Enter the number of stars?");
+  // inputs
+  numberOfStars = prompt("Enter the number of stars?");
 
-// loop
-for (let i=0; i < numberOfStars; i++ ) {
-  outputStars += "*";
-}
-
-// output 
-alert("Outputting " + numberOfStars + " stars: " + outputStars)
+ // loop
+  for (let i=0; i < numberOfStars; i++ ) {
+    outputStars += "*";
+ }
+ 
+ // output 
+ alert("Outputting " + numberOfStars + " stars: " + outputStars)
 
 // check that user entered a number and that it is greater than zero
   // otherwise ask user to enter another number
