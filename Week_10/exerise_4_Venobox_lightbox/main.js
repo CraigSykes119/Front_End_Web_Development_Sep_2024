8647970a238fd4e1b89de7821a50c1ed6750fc08
Craig Sykes <craig.sykes@musgrave.ie>
2025-01-25 20:39:51 +0000

    Unit 10 ex 4 reviewed

---
+++ Week_10/exerise_4_Venobox_lightbox/main.js
@@ -0,0 +1,9 @@
+// Initialize VenoBox
+new VenoBox({
+    selector: '.venobox',      // Targets all elements with the class 'venobox'
+    numeration: true,          // Adds slide numbers (e.g., 1/15)
+    infinigall: true,          // Enables infinite looping of the gallery
+    share: true,               // Displays social sharing buttons
+    spinner: 'pulse'           // Sets the spinner style
+  });
+  
